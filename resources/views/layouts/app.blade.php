fc5ada660d091d13c1ddb12661ce33223f8d848d
sfauzii <sjahrulfauzii@gmail.com>
2024-12-25 19:21:46 +0700

    refactor: :package: install livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,38 +4,33 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>@yield('title')</title>
+
+        {{-- Styles --}}
         @include('layouts.partials.style')
+
+        {{-- Livewire --}}
+        @livewireStyles
 
     </head>
 
     <body>
-
-        
-
-
 
         <!-- Navbar -->
         @include('layouts.partials.navbar')
 
         @yield('content')
 
+        {{-- Footer --}}
         @include('layouts.partials.footer')
 
 
+        {{-- Livewire --}}
+        @livewireScripts
 
-        <script src="frontend/scripts/main.js"></script>
-        <!-- Bootstrap CSS -->
+        {{-- Scripts --}}
+        @include('layouts.partials.scripts')
 
-        <!-- jQuery (optional, if using Bootstrap 5 with native JS, this is not needed) -->
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-        <!-- Bootstrap JS (including Popper.js) -->
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-
-        <script src="frontend/libraries/jquery/jquery-3.7.1.min.js"></script>
-        <script src="frontend/libraries/bootstrap/js/bootstrap.js"></script>
     </body>
 
 </html>
--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -1,3 +1,12 @@
-<footer>
-            <p>&copy; 2024 PintuBeasiswa. All Rights Reserved.</p>
-        </footer>
+<script src="{{ url('frontend/scripts/main.js') }}"></script>
+<!-- Bootstrap CSS -->
+
+<!-- jQuery (optional, if using Bootstrap 5 with native JS, this is not needed) -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<!-- Bootstrap JS (including Popper.js) -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="{{ url('frontend/libraries/jquery/jquery-3.7.1.min.js') }}"></script>
+<script src="{{ url('frontend/libraries/bootstrap/js/bootstrap.js') }}"></script>
